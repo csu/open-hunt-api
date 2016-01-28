f2b0c33f9c7eb2a53a03325958d24ec16d2ed23e
Christopher Su <christophersu9@gmail.com>
2016-01-28 15:38:40 -0800

    dupes

--- pyopenhunt/openhunt.py
+++ pyopenhunt/openhunt.py
@@ -22,16 +22,17 @@ def get_listings_for_date(date=None):
   items = soup.findAll('div', {'class': ['project-listing', 'item', 'row']})
 
   results = []
+  titles = []
 
   for item in items:
     title_elem = item.find('a', {'class': 'title'})
     title_text = title_elem.text.replace('\n', '')
 
-    if title_text in results:
+    if title_text in titles:
       continue
+    titles.append(title_text)
 
     item_dict = {}
-
 
     try:
       item_dict['title'] = title_text
