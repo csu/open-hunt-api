edebe839a137fad9b9d1acd77b0f8e246212d9c6
Christopher Su <christophersu9@gmail.com>
2016-01-28 16:56:39 -0800

    fix

--- server.py
+++ server.py
@@ -14,35 +14,31 @@ cache = Cache(app,config={'CACHE_TYPE': 'simple'})
 @app.route('/')
 @cache.cached(timeout=180)
 def index():
-    return jsonify({
-      'author': {
-        'name': 'Christopher Su',
-        'url': 'http://christopher.su'
-      },
-      'project': {
-        'name': 'Open Hunt API',
-        'url': 'https://github.com/csu/open-hunt-api'
-      },
-      'base_url': 'http://openhunt.christopher.su',
-      'endpoints': {
-        'today_url': 'http://openhunt.christopher.su/today',
-        'date_url': 'http://openhunt.christopher.su/{date_YYYYMMDD}'
-      }
-    })
-
-@app.route('/today')
-@cache.cached(timeout=180)
-def today():
-    return jsonify({
-      'items': pyopenhunt.get_listings_for_date()
-    })
+  return jsonify({
+    'author': {
+      'name': 'Christopher Su',
+      'url': 'http://christopher.su'
+    },
+    'project': {
+      'name': 'Open Hunt API',
+      'url': 'https://github.com/csu/open-hunt-api'
+    },
+    'base_url': 'http://openhunt.christopher.su',
+    'endpoints': {
+      'today_url': 'http://openhunt.christopher.su/today',
+      'date_url': 'http://openhunt.christopher.su/{date_YYYYMMDD}'
+    }
+  })
 
 @app.route('/<date_string>')
 @cache.cached(timeout=180)
 def specific_date(date_string):
-    return jsonify({
-      'items': pyopenhunt.get_listings_for_date(date_string)
-    })
+  if date_string == 'today':
+    date_string = None
+
+  return jsonify({
+    'items': pyopenhunt.get_listings_for_date(date_string)
+  })
 
 if __name__ == "__main__":
   port = int(os.environ.get("PORT", 5000))
